f0b8feea71b00f5b2981b0e359c7f591c89c3a4c
shohruh-xasanov <pfxasanov.shoxrux@gmail.com>
2021-11-25 00:54:13 +0500

    1

--- src/card/CardList.js
+++ src/card/CardList.js
@@ -13,14 +13,14 @@ class CardList extends Component {
     }
 
     componentDidMount () {
-        fetch('http://www.omdbapi.com/?apikey=329ffa13&s=panda')
+        fetch('https://www.omdbapi.com/?apikey=329ffa13&s=panda')
             .then(res => res.json())
             .then(data => this.setState({movies : data.Search, loading: false}))
     }
     
     SearchFilm = (str, type = 'all') => {
         this.setState({loading: true})
-        fetch(`http://www.omdbapi.com/?apikey=329ffa13&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
+        fetch(`https://www.omdbapi.com/?apikey=329ffa13&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
             .then(res => res.json())
             .then(data => this.setState({movies : data.Search, loading: false}))
     }
